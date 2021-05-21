82c59fa0065b6a4ea48f62a879f5dcae0312166c
Barry Mulrooney <43812236+mulrooneydesign@users.noreply.github.com>
2021-05-21 17:03:20 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,22 +5,54 @@ const canvas = document.querySelector('canvas.webgl')
 
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const sphereGeometry = new THREE.SphereGeometry(1, 60, 60)
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+scene.add(sphere)
+
+
+const sphere1 = new THREE.Mesh(sphereGeometry, sphereMaterial)
+sphere1.position.set(1,1, 1)
+scene.add(sphere1)
+
 
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 200)
 camera.position.z = 3
 scene.add(camera)
+
+
+
+window.addEventListener('resize', () => {
+
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+const tick = () => {
+
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+animate()
